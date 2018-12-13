b4cf182ab69ef0b59447e1a2a2b3279c42bba19d
vaishali Lodhi <vililodhi.1994@gmail.com>
2018-12-14 00:32:14 +0530

    startup

--- src/App.js
+++ src/App.js
@@ -36,101 +36,101 @@ class App extends Component {
                         </div>
                     </NavbarBrand>
                 </Navbar>
-                <div class="pusher">
-                  <div class="ui inverted vertical masthead center aligned segment">
-                    <div class="ui container">
-                      <div class="ui large secondary inverted pointing menu">
-                        <a class="toc item">
-                          <i class="sidebar icon"></i>
+                <div className="pusher">
+                  <div className="ui inverted vertical masthead center aligned segment">
+                    <div className="ui container">
+                      <div className="ui large secondary inverted pointing menu">
+                        <a className="toc item">
+                          <i className="sidebar icon"></i>
                         </a>
-                        <a class="active item">Home</a>
-                        <a class="item">Acadamic</a>
-                        <a class="item">Admission</a>
-                        <a class="item">Gallery</a>
-                        <a class="item">Contact</a>
-                        <a class="item">Notice</a>
-                        <a class="item">Careers</a>
+                        <a className="active item">Home</a>
+                        <a className="item">Acadamic</a>
+                        <a className="item">Admission</a>
+                        <a className="item">Gallery</a>
+                        <a className="item">Contact</a>
+                        <a className="item">Careers</a>
+                        <a className="item" title="See, Whats New Here?">Notice's <i className="info circle icon"></i></a>
                       </div>
                     </div>
                   </div>
 
-                  <div class="ui vertical stripe segment">
-                    <div class="ui middle aligned stackable grid container">
-                      <div class="row">
-                        <div class="eight wide column">
-                          <h3 class="ui header">We Help Companies and Companions</h3>
+                  <div className="ui vertical stripe segment">
+                    <div className="ui middle aligned stackable grid container">
+                      <div className="row">
+                        <div className="eight wide column">
+                          <h3 className="ui header">We Help Companies and Companions</h3>
                           <p>We can give your company superpowers to do things that they never thought possible. Let us delight your customers and empower your needs...through pure data analytics.</p>
-                          <h3 class="ui header">We Make Bananas That Can Dance</h3>
+                          <h3 className="ui header">We Make Bananas That Can Dance</h3>
                           <p>Yes that's right, you thought it was the stuff of dreams, but even bananas can be bioengineered.</p>
                         </div>
-                        <div class="six wide right floated column">
-                          <img src="assets/images/dnps1.jpg" class="ui large bordered rounded image" />
+                        <div className="six wide right floated column">
+                          <img src="assets/images/dnps1.jpg" className="ui large bordered rounded image" />
                         </div>
                       </div>
-                      <div class="row">
-                        <div class="center aligned column">
-                          <a class="ui huge button">Check Them Out</a>
+                      <div className="row">
+                        <div className="center aligned column">
+                          <a className="ui huge button">Check Them Out</a>
                         </div>
                       </div>
                     </div>
                   </div>
 
 
-                  <div class="ui vertical stripe quote segment">
-                    <div class="ui equal width stackable internally celled grid">
-                      <div class="center aligned row">
-                        <div class="column">
+                  <div className="ui vertical stripe quote segment">
+                    <div className="ui equal width stackable internally celled grid">
+                      <div className="center aligned row">
+                        <div className="column">
                           <h3>"What a Company"</h3>
                           <p>That is what they all say about us</p>
                         </div>
-                        <div class="column">
+                        <div className="column">
                           <h3>"I shouldn't have gone with their competitor."</h3>
                           <p>
-                            <img src="assets/images/avatar/nan.jpg" class="ui avatar image" /> <b>Nan</b> Chief Fun Officer Acme Toys
+                            <img src="assets/images/avatar/nan.jpg" className="ui avatar image" /> <b>Nan</b> Chief Fun Officer Acme Toys
                           </p>
                         </div>
                       </div>
                     </div>
                   </div>
 
-                  <div class="ui vertical stripe segment">
-                    <div class="ui text container">
-                      <h3 class="ui header">Breaking The Grid, Grabs Your Attention</h3>
+                  <div className="ui vertical stripe segment">
+                    <div className="ui text container">
+                      <h3 className="ui header">Breaking The Grid, Grabs Your Attention</h3>
                       <p>Instead of focusing on content creation and hard work, we have learned how to master the art of doing nothing by providing massive amounts of whitespace and generic content that can seem massive, monolithic and worth your attention.</p>
-                      <a class="ui large button">Read More</a>
-                      <h4 class="ui horizontal header divider">
+                      <a className="ui large button">Read More</a>
+                      <h4 className="ui horizontal header divider">
                         <a href="#">Case Studies</a>
                       </h4>
-                      <h3 class="ui header">Did We Tell You About Our Bananas?</h3>
+                      <h3 className="ui header">Did We Tell You About Our Bananas?</h3>
                       <p>Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but its really true. It took years of gene splicing and combinatory DNA research, but our bananas can really dance.</p>
-                      <a class="ui large button">I'm Still Quite Interested</a>
+                      <a className="ui large button">I'm Still Quite Interested</a>
                     </div>
                   </div>
 
 
-                  <div class="ui inverted vertical footer segment">
-                    <div class="ui container">
-                      <div class="ui stackable inverted divided equal height stackable grid">
-                        <div class="three wide column">
-                          <h4 class="ui inverted header">About</h4>
-                          <div class="ui inverted link list">
-                            <a href="#" class="item">Sitemap</a>
-                            <a href="#" class="item">Contact Us</a>
-                            <a href="#" class="item">Religious Ceremonies</a>
-                            <a href="#" class="item">Gazebo Plans</a>
+                  <div className="ui inverted vertical footer segment">
+                    <div className="ui container">
+                      <div className="ui stackable inverted divided equal height stackable grid">
+                        <div className="three wide column">
+                          <h4 className="ui inverted header">About</h4>
+                          <div className="ui inverted link list">
+                            <a href="#" className="item">Sitemap</a>
+                            <a href="#" className="item">Contact Us</a>
+                            <a href="#" className="item">Religious Ceremonies</a>
+                            <a href="#" className="item">Gazebo Plans</a>
                           </div>
                         </div>
-                        <div class="three wide column">
-                          <h4 class="ui inverted header">Services</h4>
-                          <div class="ui inverted link list">
-                            <a href="#" class="item">Banana Pre-Order</a>
-                            <a href="#" class="item">DNA FAQ</a>
-                            <a href="#" class="item">How To Access</a>
-                            <a href="#" class="item">Favorite X-Men</a>
+                        <div className="three wide column">
+                          <h4 className="ui inverted header">Services</h4>
+                          <div className="ui inverted link list">
+                            <a href="#" className="item">Banana Pre-Order</a>
+                            <a href="#" className="item">DNA FAQ</a>
+                            <a href="#" className="item">How To Access</a>
+                            <a href="#" className="item">Favorite X-Men</a>
                           </div>
                         </div>
-                        <div class="seven wide column">
-                          <h4 class="ui inverted header">Footer Header</h4>
+                        <div className="seven wide column">
+                          <h4 className="ui inverted header">Footer Header</h4>
                           <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
                         </div>
                       </div>
